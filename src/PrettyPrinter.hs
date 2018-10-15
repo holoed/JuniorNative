ab05440b1f51464c4d62f9332ec234cab393a578
holoed <edmondo@gmail.com>
2018-10-15 07:04:10 +0000

    Add reader monad to pretty printer

--- src/PrettyPrinter.hs
+++ src/PrettyPrinter.hs
@@ -4,12 +4,21 @@ import Ast
 import Fixpoint
 import RecursionSchemes
 import Text.PrettyPrint
+import Control.Monad.Reader
+
+alg (Lit (I v)) =
+   return $ text (show v)                                        
+alg (Lit (B b)) =
+   return $ text (show b)                                        
+alg (Var x) =
+   return $ text x                                                       
+alg (Lam n e) = do
+  e' <- e
+  return $ parens $ char '\\' <> text n <+> text "->" <+> e'            
+alg (App e1 e2) = do
+  e1' <- e1
+  e2' <- e2
+  return $ parens $ e1' <+> e2'
 
 pretty :: Exp -> String
-pretty = render . (cataRec alg)
-  where alg (Lit (I v)) = text (show v)
-        alg (Lit (B b)) = text (show b)
-        alg (Var x) = text x
-        alg (Lam n e) = parens $ char '\\' <> text n <+> text "->" <+> e
-        alg (App e1 e2) = parens $ e1 <+> e2
-        
+pretty = render . (\e -> runReader (cataRec alg e) 0)
