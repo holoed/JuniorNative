4a5b84b257cebb75520e5c63c9a0df391d3fb7c5
Edmondo Pentangelo <edmondo@gmail.com>
2020-02-21 07:27:20 +0000

    Continue working on Context Reduction

--- src/ContextReduction.hs
+++ src/ContextReduction.hs
@@ -5,7 +5,8 @@ import InferMonad
 import Unification
 import Substitutions
 import Monads
-import Data.Set
+import Control.Monad
+import Data.Set (toList)
 
 -- Typing Haskell in Haskell Context Reduction
 -- https://web.cecs.pdx.edu/~mpj/thih/thih.pdf
@@ -17,18 +18,27 @@ inHnf (IsIn c t) = hnf t
           hnf (TyLam _ _) = False
           hnf (TyApp t _) = hnf t
 
-tryInst :: Qual Pred -> Pred -> TypeM (Maybe (Set Pred))
+tryInst :: Qual Pred -> Pred -> TypeM (Maybe [Pred])
 tryInst (ps :=> p') p = catchError 
                (do mguPred p' p 
                    (subs, _) <- get
-                   return $ Just (substitutePredicates subs ps)) 
-                (const $ return $ Just empty)
+                   return $ Just (toList (substitutePredicates subs ps))) 
+                (const $ return $ Just [])
 
-insts :: Set (Qual Pred) -> Pred -> Set (Qual Pred)
-insts classEnv p@(IsIn c _) = 
-    Data.Set.filter (\(_ :=> (IsIn c2 _)) -> c == c2) classEnv
+insts :: [Qual Pred] -> Pred -> [Qual Pred]
+insts classEnv p@(IsIn c _) = filter (\(_ :=> (IsIn c2 _)) -> c == c2) classEnv
 
--- byInst :: Set (Qual Pred) -> Pred -> Maybe (Set Pred)
--- byInst classEnv p@(IsIn c _) =
---     let ps = Data.Set.filter (\Qual _ (IsIn c2 _) -> c == c2) classEnv
+byInst :: [Qual Pred] -> Pred -> TypeM (Maybe [Pred])
+byInst classEnv p = msum [tryInst it p | it <- insts classEnv p]
 
+toHnfs :: [Qual Pred] -> [Pred] -> TypeM [Pred]
+toHnfs classEnv ps = do pss <- mapM (toHnf classEnv) ps
+                        return (concat pss)
+
+toHnf :: [Qual Pred] -> Pred -> TypeM [Pred]
+toHnf classEnv p = do x <- byInst classEnv p 
+                      case x of
+                       Nothing -> fail "context reduction"
+                       Just ps -> toHnfs classEnv ps
+
+                       
