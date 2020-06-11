bedaab40dc1608b943cde252b754c68f4b96df01
Edmondo Pentangelo <edmondo@gmail.com>
2020-06-11 14:33:55 +0100

    Remove dependency on template haskell

--- tests/TypeInferenceTests.hs
+++ tests/TypeInferenceTests.hs
@@ -11,7 +11,6 @@ import Infer (infer)
 import Parser (parseExpr)
 import Substitutions
 import LiftNumbers
-import Data.String.Interpolate (i)
 
 env :: Env
 env = toEnv [("id", Set.fromList [] :=> TyLam (TyVar "a" 0) (TyVar "a" 0)),
@@ -41,113 +40,113 @@ classEnv = [
   Set.fromList [IsIn "Eq" (TyVar "a" 0), IsIn "Eq" (TyVar "b" 0)] :=> IsIn "Eq" (TyApp (TyApp (TyCon "Tuple") (TyVar "a" 0)) (TyVar "b" 0))
   ]
 
-typeOf :: String -> Either String (Substitutions, Qual Type)
-typeOf s = parseExpr s >>= (infer classEnv env . liftN . toExp . head)
+typeOf :: [String] -> Either String (Substitutions, Qual Type)
+typeOf s = parseExpr (unlines s) >>= (infer classEnv env . liftN . toExp . head)
 
-(-->) :: String -> String -> Expectation
+(-->) :: [String] -> String -> Expectation
 (-->) x y = either id (show . snd) (typeOf x) `shouldBe` y
 
 tests :: SpecWith ()
 tests =
   describe "Type Inference Tests" $ do
 
-    it "type of a literal 1" $ [i|42|] --> "Num a => a"
-    it "type of a literal 2" $ [i|"Hello"|] --> "String"
+    it "type of a literal 1" $ ["42"] --> "Num a => a"
+    it "type of a literal 2" $ ["\"Hello\""] --> "String"
 
     it "type of a lambda" $ 
-      [i|\\x y -> x + y|] --> "Num a => a -> a -> a"
+      ["\\x y -> x + y"] --> "Num a => a -> a -> a"
 
     it "type of a lambda 2" $
-      [i|\\x -> \\y -> x y|] --> "(a -> b) -> a -> b"
+      ["\\x -> \\y -> x y"] --> "(a -> b) -> a -> b"
         
     it "type of simple math" $ 
-      [i|12 + 24|] --> "Num a => a"
+      ["12 + 24"] --> "Num a => a"
 
     it "type of simple math 2" $
-      [i|2 * (3 + 2)|] --> "Num a => a"
+      ["2 * (3 + 2)"] --> "Num a => a"
 
     it "type of simple math 3" $
-      [i|3 - (2 / 3)|] --> "(Fractional a, Num a) => a"
+      ["3 - (2 / 3)"] --> "(Fractional a, Num a) => a"
 
     it "type of simple class constraints" $ do 
-      [i|2 > 3|] --> "Bool"
-      [i|\\x -> (x + x) < (x * x)|] --> "(Num a, Ord a) => a -> Bool"
+      ["2 > 3"] --> "Bool"
+      ["\\x -> (x + x) < (x * x)"] --> "(Num a, Ord a) => a -> Bool"
 
     it "type of a name" $ do
-      [i|id|] --> "a -> a"
-      [i|foo|] --> "Name foo not found."
+      ["id"] --> "a -> a"
+      ["foo"] --> "Name foo not found."
 
     it "type of identity" $
-      [i|\\x -> x|] --> "a -> a"
+      ["\\x -> x"] --> "a -> a"
 
     it "type of nested lam that take 2 arg and return first" $
-      [i|\\x -> \\y -> x|] --> "a -> b -> a"
+      ["\\x -> \\y -> x"] --> "a -> b -> a"
 
     it "type of composition" $
-      [i|\\f g x -> g (f x)|] --> "(a -> b) -> (b -> c) -> a -> c"
+      ["\\f g x -> g (f x)"] --> "(a -> b) -> (b -> c) -> a -> c"
 
     it "type of fmap" $
-      [i|\\f m ctx -> f (m (ctx))|] --> "(a -> b) -> (c -> a) -> c -> b"
+      ["\\f m ctx -> f (m (ctx))"] --> "(a -> b) -> (c -> a) -> c -> b"
 
     it "type of applying identity to Int" $
-      [i|id 42|] --> "Num a => a"
+      ["id 42"] --> "Num a => a"
 
     it "type of conditionals" $ do
-      [i|if True then 5 else 6|] --> "Num a => a"
-      [i|if True then 5 else False|] --> "Cannot find class instance for Num Bool"
-      [i|if True then True else 5|] -->  "Cannot find class instance for Num Bool"
-      [i|if 5 then True else False|] --> "Cannot find class instance for Num Bool"
-      [i|if \"5\" then True else False|] --> "Unable to unify String with Bool"
+      ["if True then 5 else 6"] --> "Num a => a"
+      ["if True then 5 else False"] --> "Cannot find class instance for Num Bool"
+      ["if True then True else 5"] -->  "Cannot find class instance for Num Bool"
+      ["if 5 then True else False"] --> "Cannot find class instance for Num Bool"
+      ["if \"5\" then True else False"] --> "Unable to unify String with Bool"
 
     it "type of tuple" $ do
-      [i|(2, True)|] --> "Num a => (a, Bool)"
-      [i|(False, 4)|] --> "Num a => (Bool, a)"
-      [i|\\x -> (x, x)|] --> "a -> (a, a)"
-      [i|\\x -> \\y -> (y, x)|] --> "a -> b -> (b, a)"
+      ["(2, True)"] --> "Num a => (a, Bool)"
+      ["(False, 4)"] --> "Num a => (Bool, a)"
+      ["\\x -> (x, x)"] --> "a -> (a, a)"
+      ["\\x -> \\y -> (y, x)"] --> "a -> b -> (b, a)"
 
     it "type of let" $ do
-      [i|let x = 42 in x|] --> "Num a => a"
-      [i|let pair = (True, 12) in pair|] --> "Num a => (Bool, a)"
-      [i|let x = if (True) then 2 else 3 in x + 1|] --> "Num a => a"
-      [i|let foo = \\x -> x + x in foo|] --> "Num a => a -> a"
-      [i|let foo x = x + x in foo|] --> "Num a => a -> a"
+      ["let x = 42 in x"] --> "Num a => a"
+      ["let pair = (True, 12) in pair"] --> "Num a => (Bool, a)"
+      ["let x = if (True) then 2 else 3 in x + 1"] --> "Num a => a"
+      ["let foo = \\x -> x + x in foo"] --> "Num a => a -> a"
+      ["let foo x = x + x in foo"] --> "Num a => a -> a"
 
     it "type of functions 1" $
-      [i|let f = \\x -> x in f|] --> "a -> a"
+      ["let f = \\x -> x in f"] --> "a -> a"
         
     it "type of functions 2" $
-      [i|let swap = \\p -> (snd p, fst p) in swap|] --> "(a, b) -> (b, a)"
+      ["let swap = \\p -> (snd p, fst p) in swap"] --> "(a, b) -> (b, a)"
 
     it "type of functions 3" $ 
-      [i|let fix = \\f -> f (fix f) in fix|] --> "(a -> a) -> a"
+      ["let fix = \\f -> f (fix f) in fix"] --> "(a -> a) -> a"
 
     it "type of functions 4" $ 
-      [i|let fac = \\n -> if (n == 0) then 1 else n * (fac (n - 1)) in fac|] --> "(Eq a, Num a) => a -> a"
+      ["let fac = \\n -> if (n == 0) then 1 else n * (fac (n - 1)) in fac"] --> "(Eq a, Num a) => a -> a"
 
     it "type of functions 5" $ 
-      [i|let fib n = if n == 0 then 0 else if n == 1 then 1 else fib (n - 1) + fib (n - 2) |] --> "(Eq a, Num a, Num b) => a -> b"  
+      ["let fib n = if n == 0 then 0 else if n == 1 then 1 else fib (n - 1) + fib (n - 2) "] --> "(Eq a, Num a, Num b) => a -> b"  
 
     it "type of functions 6" $ 
-      [i|let foldr f z xs = if isEmpty xs then z else f (hd xs) (foldr f z (tl xs))|] --> "(a -> b -> b) -> b -> List a -> b"
+      ["let foldr f z xs = if isEmpty xs then z else f (hd xs) (foldr f z (tl xs))"] --> "(a -> b -> b) -> b -> List a -> b"
 
     it "type of functions 6" $     
-      [i|let f x = x in (f 5, f True)|] --> "Num a => (a, Bool)"
+      ["let f x = x in (f 5, f True)"] --> "Num a => (a, Bool)"
 
     it "type of functions 7" $ 
       -- https://ghc.haskell.org/trac/ghc/blog/LetGeneralisationInGhc7
-      [i|let f x = let g y = (x, y) in (g 3, g True) in f|] --> "Num a => b -> ((b, a), (b, Bool))"
+      ["let f x = let g y = (x, y) in (g 3, g True) in f"] --> "Num a => b -> ((b, a), (b, Bool))"
 
     it "type of functions 7" $ 
-      [i|let map f xs = if isEmpty xs then empty else cons (f (hd xs)) (map f (tl xs))|] --> "(a -> b) -> List a -> List b"
+      ["let map f xs = if isEmpty xs then empty else cons (f (hd xs)) (map f (tl xs))"] --> "(a -> b) -> List a -> List b"
 
     it "type of functions 8" $
-      [i|let qsort xs = if (isEmpty xs) then xs 
-                        else concat (concat (qsort (filter (\\y -> y < hd xs) (tl xs))) 
-                                            (singleton (hd xs))) 
-                                            (qsort (filter (\\y -> y > hd xs) (tl xs))) in qsort|] --> "Ord a => List a -> List a"
+      ["let qsort xs = if (isEmpty xs) then xs", 
+       "                  else concat (concat (qsort (filter (\\y -> y < hd xs) (tl xs)))", 
+       "                                      (singleton (hd xs)))", 
+       "                                      (qsort (filter (\\y -> y > hd xs) (tl xs))) in qsort"] --> "Ord a => List a -> List a"
 
     it "Apply function with wrong tuple arity" $ do
-      [i|let f x = (fst x, snd x) in f (1, 2, 3)|] --> "Unable to unify Tuple T11 with Tuple"
+      ["let f x = (fst x, snd x) in f (1, 2, 3)"] --> "Unable to unify Tuple T11 with Tuple"
 
     it "Equality of tuples" $ do
-      [i|let f x y = (x, y) == (x, y) in f|] --> "(Eq a, Eq b) => a -> b -> Bool"
+      ["let f x y = (x, y) == (x, y) in f"] --> "(Eq a, Eq b) => a -> b -> Bool"
