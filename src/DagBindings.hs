2a718c5ff2f9d5f5e9df961168b8f8e0a8e5a87e
Edmondo Pentangelo <edmondo@gmail.com>
2021-06-15 12:40:47 +0100

    Add more dagging of let bindings

--- src/DagBindings.hs
+++ src/DagBindings.hs
@@ -1,21 +1,25 @@
 module DagBindings where
 
-import Fixpoint ( Fix(In) ) 
-import Annotations ( Ann(Ann) ) 
-import Ast (Exp, ExpF (Let, Lit, Var, MkTuple, App, Lam, Let))
+import Fixpoint ( Fix(In) )
+import Annotations ( Ann(Ann) )
+import Ast (Exp, ExpF (Let, Let))
 import Data.Set (toList)
-import Data.List (foldl)
-import RecursionSchemes ( cataRec )
 import FreeVariables (freeVars)
 import qualified Data.Graph as G
+import Data.List ( groupBy )
 
-getName :: Exp -> String 
+getName :: Exp -> String
 getName (In (Let s _ _)) = s
 getName _ = error "Expected a let binding"
 
 getDeps :: Exp -> [String]
 getDeps e = toList xs
-    where (In (Ann xs (Let _ _ _))) = freeVars e 
+    where (In (Ann xs (Let _ _ _))) = freeVars e
 
-create :: [Exp] -> [(String, [String])]
-create = foldl (\acc x -> (getName x, getDeps x) : acc) [] 
+deps :: [Exp] -> [(String, [String])]
+deps = foldl (\acc x -> (getName x, getDeps x) : acc) []
+
+chunks :: [Exp] -> [[String]]
+chunks es = reverse $ (fst <$>) <$> groupBy (\x y -> snd x == snd y) sg
+    where (g, f, _) = (G.graphFromEdges . ((\(x, y) -> (x, x, y)) <$>) . deps) es
+          sg = (\(_, y, z) -> (y, z)) . f <$> G.topSort g
--- tests/DagBindingsTests.hs
+++ tests/DagBindingsTests.hs
@@ -2,21 +2,18 @@
 module DagBindingsTests where
 
 import Parser (parseExpr)
-import DagBindings ( create )
+import DagBindings ( deps, chunks )
 import SynExpToExp ( toExp )
 import Data.String.Interpolate ( i )
-import Test.Hspec ( it, describe, shouldBe, SpecWith, Expectation )
-
-build :: String -> [(String, [String])]
-build code = create $ toExp <$> either error id (parseExpr code)
-
-(-->) :: String -> [(String, [String])] -> Expectation
-(-->) x y = build x `shouldBe` y
+import Test.Hspec ( it, describe, shouldBe, SpecWith )
 
 tests :: SpecWith ()
-tests =
-  describe "Build graph" $ do
-      
+tests = do
+  describe "Build dependencies" $ do
+
+   let build code = deps $ toExp <$> either error id (parseExpr code)
+   let (-->) x y = build x `shouldBe` y
+
    it "No deps" $ "let x = 42" --> [("x", [])]
 
    it "One dep" $ "let foo = fac 5" --> [("foo", ["fac"])]
@@ -27,6 +24,30 @@ tests =
                         let greaterThan = filter (\\x -> f x > f (hd xs)) (tl xs) in
                         (quickSort f lessThan) ++ singleton (hd xs) ++ (quickSort f greaterThan)
                     |] --> [("quicksort",["++","<",">","filter","hd","null","quickSort","singleton","tl"])]
+
+   it "Multi bindings deps" $ [i| let x = 12
+                                  let y = 32
+                                  let z = x + y |] --> [("z",["+","x","y"]),("y",[]),("x",[])]
+
+  describe "Build chunks" $ do
+
+    let build code = chunks $ toExp <$> either error id (parseExpr code)
+    let (-->) x y = build x `shouldBe` y
+
+    it "single node" $ "let x = 42" --> [["x"]]
+
+    it "two indipendent nodes" $ 
+               [i| let x = 12
+                   let y = 32 |] --> [["y","x"]]
+
+    it "two dependent nodes" $ 
+               [i| let x = 12
+                   let y = x + 1 |] --> [["x"],["y"]]
+
+    it "one node depending on two" $ 
+               [i| let x = 12
+                   let y = 32
+                   let z = x + y |] --> [["y","x"],["z"]]
                        
 
 
