dc05ac8dd0fe71f50606fb0890070064d568ab55
Edmondo Pentangelo <edmondo@gmail.com>
2021-06-17 00:19:38 +0100

    Refactor

--- src/Service.hs
+++ src/Service.hs
@@ -6,18 +6,10 @@ import qualified Data.Set as Set
 import Data.List (intercalate)
 import Data.Map (Map, empty, fromList)
 import Data.HashMap.Strict ((!))
-import Fixpoint ()
-import RecursionSchemes ()
 import Monads ()
-import Ast ()
 import Types ( Type(..), Qual((:=>)), Pred(IsIn) )
 import Environment ( Env, toEnv )
-import Infer (infer)
 import System.Console.Haskeline ()
-import Parser (parseExpr)
-import PrettyPrinter ()
-import LiftNumbers ( liftN )
-import SynExpToExp (toExp)
 import Modules (typeOfModule)
 import Data.Maybe ( fromMaybe )
 import Data.Monoid        ((<>))
@@ -25,9 +17,10 @@ import Data.Text.Lazy as Lazy ( pack )
 import Data.Text as Text (pack, unpack, replace)
 import System.Environment (lookupEnv)
 import Web.Scotty         (ActionM, ScottyM, scotty)
-import Web.Scotty.Trans ( jsonData, text, post, middleware )
+import Web.Scotty.Trans ( body, jsonData, text, post, middleware )
 import Network.Wai.Middleware.RequestLogger ( logStdoutDev )
 import Data.Aeson (FromJSON, ToJSON, Object, Value(String))
+import Data.ByteString.Lazy.Char8 as Char8 ( unpack )
 
 tyLam :: Type -> Type -> Type
 tyLam t1 = TyApp (TyApp (TyCon "->") t1)
@@ -43,7 +36,7 @@ env = toEnv [("id", Set.fromList [] :=> tyLam (TyVar "a" 0) (TyVar "a" 0)),
             ("<",  Set.fromList [IsIn "Ord" (TyVar "a" 0)] :=> tyLam (TyVar "a" 0) (tyLam (TyVar "a" 0) (TyCon "Bool"))),
             ("fst", Set.fromList [] :=> tyLam (TyApp (TyApp (TyCon "Tuple") (TyVar "a" 0)) (TyVar "b" 0)) (TyVar "a" 0)),
             ("snd", Set.fromList [] :=> tyLam (TyApp (TyApp (TyCon "Tuple") (TyVar "a" 0)) (TyVar "b" 0)) (TyVar "b" 0)),
-            ("isEmpty", Set.fromList [] :=> tyLam (TyApp (TyCon "List") (TyVar "a" 0)) (TyCon "Bool")),
+            ("null", Set.fromList [] :=> tyLam (TyApp (TyCon "List") (TyVar "a" 0)) (TyCon "Bool")),
             ("empty", Set.fromList [] :=> TyApp (TyCon "List") (TyVar "a" 0)),
             ("hd", Set.fromList [] :=> tyLam (TyApp (TyCon "List") (TyVar "a" 0)) (TyVar "a" 0)),
             ("tl", Set.fromList [] :=> tyLam (TyApp (TyCon "List") (TyVar "a" 0)) (TyApp (TyCon "List") (TyVar "a" 0))),
@@ -58,7 +51,7 @@ classEnv = [
 
 main :: IO ()
 main = do
-  putStrLn "Junior Service started"
+  Prelude.putStrLn "Junior Service started"
   pStr <- fromMaybe "8080" <$> lookupEnv "PORT"
   let p = read pStr :: Int
   scotty p route
@@ -67,8 +60,7 @@ route :: ScottyM()
 route = do
     middleware logStdoutDev
     post "/" $ do
-         body <- jsonData :: ActionM Object
-         let code = fromJson $ body ! Text.pack "code"
-         text $ Lazy.pack $ format (typeOfModule classEnv env code) ++ "\r\n"
+         code <- body
+         text $ Lazy.pack $ format (typeOfModule classEnv env (Char8.unpack code)) ++ "\r\n"
   where fromJson (String s) = Text.unpack (Text.replace "\\n" "\n" s)
-        format = intercalate "\n" . ((\(n, t) -> n ++ ": " ++ t) <$>)
+        format = Data.List.intercalate "\n" . ((\(n, t) -> n ++ ": " ++ t) <$>)
