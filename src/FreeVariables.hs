9d86d9f9e9af8b431e463ab096d4eb16763c01b0
Edmondo Pentangelo <edmondo@gmail.com>
2021-06-26 14:22:41 +0100

    Fix FreeVars TuplePat support

--- src/FreeVariables.hs
+++ src/FreeVariables.hs
@@ -10,45 +10,51 @@ import Control.Monad.Writer ( runWriter, MonadWriter(listen, tell, pass), Writer
 type FreeVarsM = Writer (Set String)
 type FreeVarsExp = Fix (Ann (Maybe Loc, Set String) ExpF)
 
-getName :: FreeVarsExp -> String 
-getName (In (Ann (_, _) (VarPat s))) = s
+getNames :: FreeVarsExp -> [String]
+getNames (In (Ann (_, _) (VarPat s))) = [s]
+getNames (In (Ann (_, _) (TuplePat ss))) = ss >>= getNames
 
 freeVars :: Set String -> Exp -> Fix (Ann (Maybe Loc, Set String) ExpF)
 freeVars globals e = fst $ runWriter (cataRec alg e)
   where
     alg :: Ann (Maybe Loc) ExpF (FreeVarsM FreeVarsExp) -> FreeVarsM FreeVarsExp
     alg (Ann l (Lit x)) = return $ In (Ann (l, empty) (Lit x))
-    alg (Ann l (Var s)) = do 
+    alg (Ann l (Var s)) = do
       let fv = if member s globals
                then empty
                else singleton s
       tell fv
       return $ In (Ann (l, fv) (Var s))
-    alg (Ann l (VarPat s)) = do 
+    alg (Ann l (VarPat s)) = do
       let fv = if member s globals
                then empty
                else singleton s
       tell fv
       return $ In (Ann (l, fv) (VarPat s))
-    alg (Ann l (MkTuple xs)) = do 
+    alg (Ann l (MkTuple xs)) = do
       (xs', fvs) <- listen (sequence xs)
       return $ In (Ann (l, fvs) $ MkTuple xs')
-    alg (Ann l (App e1 e2)) = do 
+    alg (Ann l (TuplePat xs)) = do
+      (xs', fvs) <- listen (sequence xs)
+      return $ In (Ann (l, fvs) $ TuplePat xs')
+    alg (Ann l (App e1 e2)) = do
       (e1', fvs1) <- listen e1
       (e2', fvs2) <- listen e2
       return $ In (Ann (l, fvs1 `union` fvs2) $ App e1' e2')
-    alg (Ann l (Lam n e')) = pass (do 
+    alg (Ann l (Lam n e')) = pass (do
       n' <- n
-      let s' = getName n'
+      let s' = getNames n'
       (e'', fvs) <- listen e'
-      return (In (Ann (l, delete s' fvs) $ Lam n' e''), delete s'))
+      let f x = foldl (flip delete) x s'
+      return (In (Ann (l, f fvs) $ Lam n' e''), f))
     alg (Ann l (Let n v b)) = pass (do 
        n' <- n
-       let s' = getName n'
+       let s' = getNames n'
        (v', fvs1) <- listen v
        (b', fvs2) <- listen b
-       return (In (Ann (l, delete s' $ fvs1 `union` fvs2) $ Let n' v' b'), delete s'))
-    alg (Ann l (IfThenElse p e1 e2)) = do 
+       let f x = foldl (flip delete) x s'
+       return (In (Ann (l, f $ fvs1 `union` fvs2) $ Let n' v' b'), f))
+    alg (Ann l (IfThenElse p e1 e2)) = do
       (p', fvs1) <- listen p
       (e1',fvs2) <- listen e1
       (e2',fvs3) <- listen e2
