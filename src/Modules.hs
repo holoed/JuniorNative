c9303c2e3a569d8d5c816f6ff24a8350967d57e5
Edmondo Pentangelo <edmondo@gmail.com>
2021-06-30 13:59:33 +0100

    Preparing to parmap bindings in the same dep group

--- src/Modules.hs
+++ src/Modules.hs
@@ -3,7 +3,7 @@ module Modules where
 import Ast ( Exp, ExpF(Let, VarPat) )
 import Fixpoint ( Fix(In) )
 import Infer ( infer )
-import Environment ( Env, toEnv )
+import Environment ( Env, toEnv, concatEnvs )
 import Data.Map as Map ( keysSet, fromList, toList, (!), union, restrictKeys, Map )
 import Data.Set as Set (fromList)
 import Data.List (sortOn)
@@ -13,21 +13,27 @@ import Parser ( parseExpr )
 import DagBindings ( chunks )
 import LiftNumbers ( liftN )
 import Annotations (Ann(..))
-import ContextReduction (ClassEnv)
+import ContextReduction (ClassEnv, Class)
 
 bindingsDict :: [Exp] -> Map String Exp 
 bindingsDict es = Map.fromList ((\e@(In (Ann _ (Let (In (Ann _ (VarPat s))) _ _))) -> (s, e)) <$> es)
 
+foo :: [[(String, Exp)]] -> ClassEnv -> Env -> Env
+foo bss classEnv env = 
+      foldl (\acc bs -> foldl concatEnvs acc $ map (f acc) bs) env bss
+   where
+     f env' (n, e) = 
+       let t = (either (\err -> Set.fromList [] :=> TyCon err) snd . infer classEnv env' . liftN) e in
+       toEnv [(n, t)] 
+
 typeOfModule :: ClassEnv -> Env -> String -> [(String, String)] 
 typeOfModule classEnv env x = sortedRet
     where es = either error (toExp <$>) (parseExpr x)
-          ns = (fst <$>) $ concat $ chunks (Map.keysSet env) es
+          ns = (fst <$>) <$> chunks (Map.keysSet env) es
           dict = bindingsDict es
-          bs = (\n -> (n, dict!n)) <$> ns
-          f env' (n, e) = 
-             let t = (either (\err -> Set.fromList [] :=> TyCon err) snd . infer classEnv env' . liftN) e in
-             toEnv [(n, t)] `union` env'    
-          finalEnv = restrictKeys (foldl f env bs) (Set.fromList ns)
+          bs = ((\n -> (n, dict!n)) <$>) <$> ns
+          flattenedNs = concat ns
+          finalEnv = restrictKeys (foo bs classEnv env) (Set.fromList flattenedNs)
           unsortedRet = (\(n, ForAll _ qt) -> (n, show qt)) <$> Map.toList finalEnv
-          orderDict = Map.fromList (zip ns ([1..] :: [Int]))
+          orderDict = Map.fromList (zip flattenedNs ([1..] :: [Int]))
           sortedRet = sortOn ((orderDict!) . fst) unsortedRet
