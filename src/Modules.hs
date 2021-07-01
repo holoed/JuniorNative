0ed95ee2514920d3ce7ee25af2123351795705e6
Edmondo Pentangelo <edmondo@gmail.com>
2021-07-01 12:39:13 +0100

    Propagate errors

--- src/Modules.hs
+++ src/Modules.hs
@@ -7,7 +7,7 @@ import Environment ( Env, toEnv, concatEnvs )
 import Data.Map as Map ( keysSet, fromList, toList, (!), restrictKeys, Map )
 import Data.Set as Set (fromList)
 import Data.List (sortOn)
-import Types ( Qual((:=>)), Type(TyCon), TypeScheme(ForAll) )
+import Types ( TypeScheme(ForAll) )
 import SynExpToExp ( toExp )
 import Parser ( parseExpr )
 import DagBindings ( chunks )
@@ -19,28 +19,27 @@ import Control.Parallel.Strategies (parMap, rdeepseq)
 bindingsDict :: [Exp] -> Map String Exp
 bindingsDict es = Map.fromList ((\e@(In (Ann _ (Let (In (Ann _ (VarPat s))) _ _))) -> (s, e)) <$> es)
 
-inferTypesFromDaggedBindings :: [[(String, Exp)]] -> ClassEnv -> Env -> Env
+inferTypesFromDaggedBindings :: [[(String, Exp)]] -> ClassEnv -> Either String Env -> Either String Env
 inferTypesFromDaggedBindings bss classEnv env =
-      foldl (\acc bs -> foldl concatEnvs acc $ parMap rdeepseq (f acc) bs) env bss
+      foldl (\acc bs -> foldl (\ev1 ev2 -> concatEnvs <$> ev1 <*> ev2) acc $ parMap rdeepseq (f acc) bs) env bss
    where
-     f env' (n, e) =
-       let t = (either (\err -> Set.fromList [] :=> TyCon err) snd . infer classEnv env' . liftN) e in
-       toEnv [(n, t)]
+     g env' (n, e) = (\t -> toEnv [(n, t)]) . snd <$> (infer classEnv env' . liftN) e
+     f env' (n, e) = env' >>= flip g (n, e)
 
-
-inferTypes :: [Exp] -> ClassEnv -> Env -> [(String, String)]
-inferTypes es classEnv env  = sortOn ((orderDict!) . fst) unsortedRet
+inferTypes :: [Exp] -> ClassEnv -> Env -> Either String [(String, String)]
+inferTypes es classEnv env  = sortOn ((orderDict!) . fst) <$> unsortedRet
   where
           ns = (fst <$>) <$> chunks (Map.keysSet env) es
           dict = bindingsDict es
           bs = ((\n -> (n, dict!n)) <$>) <$> ns
           flattenedNs = concat ns
-          finalEnv = restrictKeys (inferTypesFromDaggedBindings bs classEnv env) (Set.fromList flattenedNs)
-          unsortedRet = (\(n, ForAll _ qt) -> (n, show qt)) <$> Map.toList finalEnv
+          env' = inferTypesFromDaggedBindings bs classEnv (Right env)
+          finalEnv = flip restrictKeys (Set.fromList flattenedNs) <$> env'
+          unsortedRet = fmap (\ (n, ForAll _ qt) -> (n, show qt)) . toList <$> finalEnv
           orderDict = Map.fromList (zip flattenedNs ([1..] :: [Int]))
 
 
 typeOfModule :: ClassEnv -> Env -> String -> Either String [(String, String)]
 typeOfModule classEnv env x =
-    (\es' -> inferTypes es' classEnv env) <$> ((toExp <$>) <$> parseExpr x)
+    parseExpr x >>= (\es' -> inferTypes es' classEnv env) . (toExp <$>)
   
