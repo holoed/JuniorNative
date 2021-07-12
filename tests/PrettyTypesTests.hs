d22f89ddbfc774b9f786fe95620e90afb457e4c3
Edmondo Pentangelo <edmondo@gmail.com>
2021-07-12 23:18:54 +0100

    Add more tests

--- tests/PrettyTypesTests.hs
+++ tests/PrettyTypesTests.hs
@@ -2,11 +2,11 @@ module PrettyTypesTests where
 
 import Test.Hspec (SpecWith, shouldBe, describe, it, Expectation)
 import PrettyTypes (prettyQ)
-import Types (Type(..), Qual(..), tyLam)
+import Types (Type(..), Qual(..), Pred(..), tyLam)
 import TypesPrinter ()
 import Data.Set (fromList)
 
-(-->) :: Qual Type -> String -> Expectation 
+(-->) :: Qual Type -> String -> Expectation
 (-->) qt s = show qt `shouldBe` s
 
 tests :: SpecWith ()
@@ -21,4 +21,15 @@ tests =
 
     it "Pretty a function" $ do
         prettyQ (fromList [] :=> tyLam (TyVar "T1" 0) (TyVar "T2" 0)) --> "a -> b"
-        
+
+    it "Pretty a type application" $ do
+        prettyQ (fromList [] :=> TyApp (TyVar "T1" 1) (TyVar "T2" 0)) --> "a b"
+
+    it "Pretty type parenthesized" $ do
+        prettyQ (fromList [] :=> tyLam (tyLam (TyVar "T1" 0) (TyVar "T2" 0)) (TyVar "T3" 0)) --> "(a -> b) -> c"
+
+    it "Pretty type with type class constraint" $ do
+        prettyQ (fromList [IsIn "Num" (TyVar "T1" 0)] :=> TyVar "T1" 0) --> "Num a => a"
+
+    it "Pretty type with multiple type class constraints" $ do
+        prettyQ (fromList [IsIn "Num" (TyVar "T1" 0), IsIn "Ord" (TyVar "T1" 0)] :=> TyVar "T1" 0) --> "(Num a, Ord a) => a"
