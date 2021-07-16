447ab75b616cfb6931e12b6d89e23e58ce68e778
Edmondo Pentangelo <edmondo@gmail.com>
2021-07-16 15:18:32 +0100

    Refactor

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Interpreter where
 
-import Location ( PString )
+import Location ( PString(..) )
 import Annotations ( unwrap )
 import Ast (Exp, ExpF(..))
+import Control.Monad.Error.Class (MonadError(..))
 import Control.Monad.Reader ( MonadReader(ask, local), runReader, Reader )
 import Control.Monad.Trans.Except ( ExceptT, runExceptT )
 import RecursionSchemes (cataRec)
@@ -11,31 +13,31 @@ import Primitives ( Prim(..) )
 import Control.Monad.Fail ()
 
 type Env = Map String Result
-type InterpreterM = ExceptT PString (Reader Env) 
+newtype InterpreterM a = InterpreterM { runMonad :: ExceptT PString (Reader Env) a }
+   deriving ( Functor, Applicative, Monad, MonadReader Env, MonadError PString )
+
+instance MonadFail InterpreterM where
+  fail s = throwError (PStr (s, Nothing)) 
 
 data Result = Value Prim
             | Function (Result -> InterpreterM Result)
  
 interpret :: Env -> Exp -> Either PString Result
-interpret env =  flip runReader env . runExceptT  . cataRec alg . unwrap
+interpret env =  flip runReader env . runExceptT  . runMonad .cataRec alg . unwrap
     where alg (Lit x) = return $ Value x
           alg (Var n) = do ctx <- ask
                            return $ ctx!n
           alg (VarPat n) = return $ Value (S n)
           alg (Lam e1 e2) = do ctx <- ask
-                               nr <- e1
-                               let (Value (S n)) = nr
+                               (Value (S n))  <- e1
                                return $ Function (\r -> local (\ctx' -> insert n r ctx' `union` ctx) e2)
-          alg (Let e1 e2 e3) = do nr <- e1
-                                  let (Value (S n)) = nr
+          alg (Let e1 e2 e3) = do (Value (S n)) <- e1
                                   v' <- e2
                                   local (insert n v') e3
-          alg (App e1 e2) = do rf <- e1
-                               let (Function f) = rf
+          alg (App e1 e2) = do (Function f) <- e1
                                x <- e2
                                f x
-          alg (IfThenElse e1 e2 e3) = do rp <- e1
-                                         let (Value (B b)) = rp
+          alg (IfThenElse e1 e2 e3) = do (Value (B b)) <- e1
                                          if b then e2 else e3
           alg _ = undefined
 
