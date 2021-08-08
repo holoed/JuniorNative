627b4a2893530815b73392671b7d1e76edf0c435
Edmondo Pentangelo <edmondo@gmail.com>
2021-08-08 14:44:35 +0100

    Add more tests

--- tests/CompilerTests.hs
+++ tests/CompilerTests.hs
@@ -250,3 +250,5 @@ tests = do
 
    it "Calculator Test" $ "tests/jnrs_lib/calculator.jnr" ---> "[(5,[])]"
 
+   it "Calculator Test 2" $ "tests/jnrs_lib/calculator_with_spaces.jnr" ---> "[(14,[])]"
+
