2ed3af017ce47b065a10399730f381e8f59815fe
Edmondo Pentangelo <edmondo@gmail.com>
2021-09-15 23:28:07 +0100

    Update service to use ANF

--- src/Service.hs
+++ src/Service.hs
@@ -17,7 +17,7 @@ import Data.ByteString.Lazy.Char8 as Char8 ( unpack )
 import Intrinsics ( env, classEnv )
 import Data.Aeson
     ( ToJSON(toJSON), object, KeyValue((.=)) )
-import Compiler (fullInterp, backendPrinted, frontEndPrinted, fullJSClosed)
+import Compiler (fullInterp, backendPrinted, frontEndPrinted, fullJSClosedANF)
 import CompilerMonad (CompileM, run)
 import qualified SymbolTable as S
 import qualified InterpreterIntrinsics as Interp (env)
@@ -74,7 +74,7 @@ route = do
          either json json ret
     post "/compileToJs" $ do
          code <- body
-         ret <- liftIO $ compile fullJSClosed (Char8.unpack code)
+         ret <- liftIO $ compile fullJSClosedANF (Char8.unpack code)
          either json json ret
     get "/libJs" $ do
          ret <- liftIO $ readFile "src/javascript/baseClosedLib.js"
