f8f4ed301cf403a8d1a04864cbbdccd1344331e7
Edmondo Pentangelo <edmondo@gmail.com>
2022-04-08 00:31:08 +0100

    Refactor

--- src/compiler/OptimizeTypeClasses.hs
+++ src/compiler/OptimizeTypeClasses.hs
@@ -5,13 +5,13 @@ import Fixpoint (Fix(In))
 import Control.Monad.State.Lazy (evalState, MonadState (put, get), State)
 import RecursionSchemes (cataRec)
 import Annotations (Ann(Ann))
-import Ast (ExpF(VarPat, AppClosure, Var, Defn, Let))
+import Ast (ExpF(VarPat, AppClosure, Var, Let))
 import Data.Map ( insert, empty, member, Map, (!) )
 import Debug.Trace (trace)
 import Prelude hiding (lookup)
 import Control.Monad ((>=>))
 
-type OptimizeM = State (Map String String)
+type OptimizeM = State (Map String TypedExp)
 
 optimizeImp :: [TypedExp] -> OptimizeM [TypedExp]
 optimizeImp es = sequence (cataRec alg <$> es)
@@ -21,16 +21,16 @@ optimizeImp es = sequence (cataRec alg <$> es)
             n' <- n
             v' <- v
             case (n', v') of
-             (In (Ann _ (VarPat k)) ,In (Ann _ (Var "=="))) -> do
+             (In (Ann _ (VarPat k)) ,In (Ann attr2 (Var "=="))) -> do
                  state <- get
-                 put (insert k "nativeEq" state)
+                 put (insert k (In (Ann attr2 (Var "nativeEq"))) state)
                  b' <- b
                  return $ trace "match" $ In (Ann attr (Let n' v' b'))
              (_, _) -> In . Ann attr . Let n' v' <$> b
         alg (Ann attr (Var n)) = do
             state <- get
             if member n state
-            then return $ trace "replaced" $ In (Ann attr (Var (state!n)))
+            then return $ trace "replaced" $ state!n
             else return $ In (Ann attr (Var n))
         alg (Ann attr (AppClosure e1 e2)) = do
             e1' <- e1
