d7671e340194b3d82cdecb7d69b54df655ae4aaa
Edmondo Pentangelo <edmondo@gmail.com>
2022-04-08 22:30:18 +0100

    Continue optimization transformation

--- src/compiler/OptimizeTypeClasses.hs
+++ src/compiler/OptimizeTypeClasses.hs
@@ -2,16 +2,17 @@ module OptimizeTypeClasses where
 
 import TypedAst (TypedExp, TypedExpF)
 import Fixpoint (Fix(In))
-import Control.Monad.State.Lazy (evalState, MonadState (put, get), State)
+import Control.Monad.Trans.Reader ( ask, local, ReaderT(runReaderT) )
+import Control.Monad.State ( evalState, State, MonadState(put, get) )
 import RecursionSchemes (cataRec)
 import Annotations (Ann(Ann))
-import Ast (ExpF(VarPat, AppClosure, Var, Let))
-import Data.Map ( insert, empty, member, Map, (!) )
+import Ast (ExpF(VarPat, AppClosure, Var, Let, MkTuple, App))
+import Data.Map ( insert, empty, member, Map, (!), delete )
 import Debug.Trace (trace)
 import Prelude hiding (lookup)
 import Control.Monad ((>=>))
 
-type OptimizeM = State (Map String TypedExp)
+type OptimizeM = ReaderT (Map String TypedExp) (State (Map String TypedExp))
 
 optimizeImp :: [TypedExp] -> OptimizeM [TypedExp]
 optimizeImp es = sequence (cataRec alg <$> es)
@@ -21,26 +22,38 @@ optimizeImp es = sequence (cataRec alg <$> es)
             n' <- n
             v' <- v
             case (n', v') of
-             (In (Ann _ (VarPat k)) ,In (Ann attr2 (Var "=="))) -> do
+             (In (Ann _ (VarPat k)) ,In (Ann _ (Var "nativeEqInt"))) -> do
                  state <- get
-                 put (insert k (In (Ann attr2 (Var "nativeEq"))) state)
-                 b' <- b
-                 return $ trace "match" $ In (Ann attr (Let n' v' b'))
+                 put (insert k v' state)
+                 b' <- local (insert k v') b
+                 return $ trace (k ++ " to be inlined") $ In (Ann attr (Let n' v' b'))
+             (In (Ann _ (VarPat k)) , In (Ann _ (AppClosure (In (Ann _ (Var "nativeEqInt"))) _))) -> do
+                 state <- get
+                 put (insert k v' state)
+                 b' <- local (insert k v') b
+                 return $ trace (k ++ " to be inlined") $ In (Ann attr (Let n' v' b'))
+             (In (Ann _ (VarPat k)) , _) -> do
+                 b' <- local (insert k v') b
+                 return $ trace (k ++ " in context") $ In (Ann attr (Let n' v' b'))
              (_, _) -> In . Ann attr . Let n' v' <$> b
-        alg (Ann attr (Var n)) = do
-            state <- get
-            if member n state
-            then return $ trace "replaced" $ state!n
-            else return $ In (Ann attr (Var n))
         alg (Ann attr (AppClosure e1 e2)) = do
             e1' <- e1
             e2' <- e2
             case (e1', e2') of
+                (In (Ann _ (AppClosure l@(In (Ann _ (Var "nativeEqInt"))) e3)), _) ->
+                    return (In (Ann attr (App l (In (Ann attr (MkTuple [e3,e2']))))))
                 (In (Ann _ (Var "==")), In (Ann _ (Var "eqInt"))) ->
-                    return $ trace "Optimized __eqeq" $ In (Ann attr (Var "=="))
+                    return $ trace "Optimized __eqeq" $ In (Ann attr (Var "nativeEqInt"))
+                (In (Ann _ (Var n1)), _) -> do
+                    state <- get
+                    if member n1 state
+                    then do
+                        put $ delete n1 state
+                        return $ trace ("inlined " ++ n1) $ In (Ann attr (AppClosure (state!n1) e2'))
+                    else return $ In (Ann attr (AppClosure e1' e2'))
                 (_, _) ->
                     return $ In (Ann attr (AppClosure e1' e2'))
         alg x = fmap In (sequenceA x)
 
 optimize :: [TypedExp] -> [TypedExp]
-optimize es = evalState ((optimizeImp >=> optimizeImp) es) empty
+optimize es = evalState (runReaderT ((optimizeImp >=> optimizeImp) es) empty) empty
