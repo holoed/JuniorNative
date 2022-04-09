759d5d60afb657fb95c5d034edfaa55145e457db
Edmondo Pentangelo <edmondo@gmail.com>
2022-04-09 23:04:00 +0100

    Deadcode eliminate after optimize

--- tests/OptimizeTypeClassesTests.hs
+++ tests/OptimizeTypeClassesTests.hs
@@ -8,12 +8,13 @@ import Intrinsics (env, classEnv)
 import qualified InterpreterIntrinsics as Interp (env)
 import Data.Text (unpack, Text)
 import Data.String.Interpolate (i)
-import CompilerSteps (optimizeTypeClasses, prettyPrintModule)
+import CompilerSteps (optimizeTypeClasses, deadCodeElimin, prettyPrintModule)
 import Control.Monad ((>=>))
 
 compile :: String -> CompileM Text
 compile = closedAndANF >=>
        step "Optimize away fully resolved type classes instances" optimizeTypeClasses >=>
+       step "Dead code elimination" deadCodeElimin >=>
        step "pretty print module" prettyPrintModule
 
 build :: String -> IO String
@@ -29,25 +30,23 @@ tests = do
   describe "Optimize resolved Type Classes Known Instances" $ do
 
    it "native eq" $ "let main = 2 == 3" --> [i|val main :: Bool
-let main = let anf_2 = nativeEqInt in
-           let anf_0 = AppClosure (fromInteger, numInt) in
-           let anf_1 = 2 in
-           let anf_3 = AppClosure (anf_0, anf_1) in
-           let anf_6 = AppClosure (nativeEqInt, anf_3) in
-           let anf_4 = AppClosure (fromInteger, numInt) in
-           let anf_5 = 3 in
-           let anf_7 = AppClosure (anf_4, anf_5) in
-           nativeEqInt (anf_3, anf_7)
+let main = 
+    let anf_0 = AppClosure (fromInteger, numInt) in
+    let anf_1 = 2 in
+    let anf_3 = AppClosure (anf_0, anf_1) in
+    let anf_4 = AppClosure (fromInteger, numInt) in
+    let anf_5 = 3 in
+    let anf_7 = AppClosure (anf_4, anf_5) in
+    nativeEqInt (anf_3, anf_7)
 |]
 
    it "native plus" $ "let main = 2 + 3" --> [i|val main :: Int
-let main = let anf_2 = nativeAddInt in
-           let anf_0 = AppClosure (fromInteger, numInt) in
-           let anf_1 = 2 in
-           let anf_3 = AppClosure (anf_0, anf_1) in
-           let anf_6 = AppClosure (nativeAddInt, anf_3) in
-           let anf_4 = AppClosure (fromInteger, numInt) in
-           let anf_5 = 3 in
-           let anf_7 = AppClosure (anf_4, anf_5) in
-           nativeAddInt (anf_3, anf_7)
+let main = 
+    let anf_0 = AppClosure (fromInteger, numInt) in
+    let anf_1 = 2 in
+    let anf_3 = AppClosure (anf_0, anf_1) in
+    let anf_4 = AppClosure (fromInteger, numInt) in
+    let anf_5 = 3 in
+    let anf_7 = AppClosure (anf_4, anf_5) in
+    nativeAddInt (anf_3, anf_7)
 |]
