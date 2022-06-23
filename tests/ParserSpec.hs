b3c5ccf5b1e4421a9fd74a06bdca477984031e1f
Edmondo Pentangelo <edmondo@gmail.com>
2022-06-23 09:12:09 +0100

    Add more Parser Tests

--- tests/ParserSpec.hs
+++ tests/ParserSpec.hs
@@ -7,7 +7,7 @@ import Test.Hspec (Spec, shouldBe, describe, it, parallel)
 import Annotations (Ann(Ann))
 import Location (Loc (Loc), PString (PStr))
 import Fixpoint (Fix(In))
-import PAst (SynExpF(Lit, VarPat, Defn, InfixApp, Var, TypeDecl, Match, MatchExp))
+import PAst (SynExpF(Lit, VarPat, Defn, InfixApp, Var, TypeDecl, Match, MatchExp, TuplePat))
 import Primitives (Prim(I))
 import Types (Type(TyCon, TyApp, TyVar), Qual((:=>)), Pred (IsIn), tyLam)
 import Data.Set (fromList)
@@ -101,7 +101,19 @@ spec = parallel $ do
                     | y -> y
                     | z -> z
      |] `shouldBe` 
-      Right  [In (Ann (Just (Loc 3 2 9)) (Defn Nothing [In (Ann (Just (Loc 3 2 13)) (VarPat "foo")),(In (Ann (Just (Loc 1 2 17)) (VarPat "x")))] (In (Ann (Just (Loc 5 2 21)) (Match (In (Ann (Just (Loc 1 2 27)) (Var "x"))) [
+      Right  [In (Ann (Just (Loc 3 2 9)) (Defn Nothing [In (Ann (Just (Loc 3 2 13)) (VarPat "foo")),(In (Ann (Just (Loc 1 2 17)) (VarPat "x")))] 
+        (In (Ann (Just (Loc 5 2 21)) (Match (In (Ann (Just (Loc 1 2 27)) (Var "x"))) [
         (In (Ann (Just (Loc 2 3 25)) (MatchExp (In (Ann (Just (Loc 1 3 23)) (VarPat "y"))) (In (Ann (Just (Loc 1 3 28)) (Var "y")))))),
         (In (Ann (Just (Loc 2 4 25)) (MatchExp (In (Ann (Just (Loc 1 4 23)) (VarPat "z"))) (In (Ann (Just (Loc 1 4 28)) (Var "z"))))))
         ])))))]
+
+  it "pattern matching syntax 3" $
+     parseExpr [i|
+        let foo x = match x with 
+                    | (y, z) -> z
+                    
+     |] `shouldBe` 
+      Right   [(In (Ann (Just (Loc 3 2 9)) (Defn Nothing [In (Ann (Just (Loc 3 2 13)) (VarPat "foo")),(In (Ann (Just (Loc 1 2 17)) (VarPat "x")))] 
+               (In (Ann (Just (Loc 5 2 21)) (Match (In (Ann (Just (Loc 1 2 27)) (Var "x"))) [
+               (In (Ann (Just (Loc 2 3 30)) (MatchExp (In (Ann (Just (Loc 1 3 23)) (TuplePat [In (Ann (Just (Loc 1 3 24)) (VarPat "y")),(In (Ann (Just (Loc 1 3 27)) (VarPat "z")))]))) (In (Ann (Just (Loc 1 3 33)) (Var "z"))))))
+                   ]))))))]
